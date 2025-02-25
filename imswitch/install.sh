be32191205a27737c489e21f120b2eb05293ad46
Ethan Li <lietk12@gmail.com>
2025-02-25 09:04:26 -0800

    Work around incorrect file perm flags in the ImSwitchDockerInstall repo

--- imswitch/install.sh
+++ imswitch/install.sh
@@ -6,43 +6,43 @@ cd ~/Downloads
 git clone https://github.com/openUC2/ImSwitchDockerInstall
 cd ImSwitchDockerInstall
 
-# install requirements 
+# install requirements
 sudo apt-get install -y git curl
 
 echo "Pull and Install Docker Image"
-chmod +x pull_and_run.sh
+sudo chmod +x pull_and_run.sh
 ./pull_and_run.sh
 
 echo "Clone ImSwitchConfig"
-chmod +x git_clone_imswitchconfig.sh
+sudo chmod +x git_clone_imswitchconfig.sh
 ./git_clone_imswitchconfig.sh
 
 echo "Install HIK Driver"
-chmod +x install_hikdriver.sh
+sudo chmod +x install_hikdriver.sh
 ./install_hikdriver.sh
 
 echo "Install Daheng Driver"
-chmod +x install_dahengdriver.sh
+sudo chmod +x install_dahengdriver.sh
 ./install_dahengdriver.sh
 
 echo "Install Vimba Driver"
-chmod +x install_vimba.sh
+sudo chmod +x install_vimba.sh
 ./install_vimba.sh
 
 echo "Setup RaspAp"
-chmod +x install_raspap.sh
+sudo chmod +x install_raspap.sh
 ./install_raspap.sh
 
 echo "Create Desktop Icons"
-chmod +x create_desktopicons.sh
+sudo chmod +x create_desktopicons.sh
 ./create_desktopicons.sh
 
 echo "Set install_autostart for ImSwitch"
-chmod +x install_autostart.sh
+sudo chmod +x install_autostart.sh
 ./install_autostart.sh
 
 # add serial devices to user group
-sudo usermod -a -G dialout $USER
-sudo usermod -a -G tty $USER
+sudo usermod -a -G dialout "$USER"
+sudo usermod -a -G tty "$USER"
 echo "Please reboot to take effect of adding serial devices to user group"
- 
+

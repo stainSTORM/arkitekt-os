739bf76750b821d9a49d0b253a137a11fc659133
Ethan Li <lietk12@gmail.com>
2025-02-25 13:45:01 -0800

    Bump ImSwitchDockerInstall

--- imswitch/install.sh
+++ imswitch/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
@@ -16,36 +16,25 @@ git checkout --quiet "$installer_version"
 # install requirements
 sudo apt-get install -y git curl
 
-#echo "Pull and Install Docker Image"
-#sudo chmod +x pull_and_run.sh
-#./pull_and_run.sh
-
 echo "Clone ImSwitchConfig"
-sudo chmod +x git_clone_imswitchconfig.sh
 ./git_clone_imswitchconfig.sh
 
 echo "Install HIK Driver"
-sudo chmod +x install_hikdriver.sh
 ./install_hikdriver.sh
 
 echo "Install Daheng Driver"
-sudo chmod +x install_dahengdriver.sh
 ./install_dahengdriver.sh
 
 echo "Install Vimba Driver"
-sudo chmod +x install_vimba.sh
 ./install_vimba.sh
 
 echo "Setup RaspAp"
-sudo chmod +x install_raspap.sh
 ./install_raspap.sh
 
 echo "Create Desktop Icons"
-sudo chmod +x create_desktopicons.sh
 ./create_desktopicons.sh
 
 echo "Set install_autostart for ImSwitch"
-sudo chmod +x install_autostart.sh
 ./install_autostart.sh
 
 # add serial devices to user group

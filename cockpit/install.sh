ce0d7971f0d258ef16cda67c2ba93acdc2b2bd0f
Ethan Li <lietk12@gmail.com>
2025-04-14 17:49:48 -0700

    Move Cockpit/imswitch integration into the pallet (#8)
    * Move Cockpit/imswitch integration into the pallet
    
    * Bump pallet

--- cockpit/install.sh
+++ cockpit/install.sh
@@ -11,35 +11,4 @@ ls
 sudo apt-get install -y /tmp/cockpit-navigator.deb
 rm /tmp/cockpit-navigator.deb
 
-# Add a minimal Cockpit package for IMSwitch
-sudo mkdir -p /usr/share/cockpit/imswitch
-
-# Create a manifest.json to list IMSwitch under "Tools"
-cat << 'EOF' | sudo tee /usr/share/cockpit/imswitch/manifest.json > /dev/null
-{
-  "version": 0,
-  "tools": {
-    "imswitch": {
-      "label": "IMSwitch",
-      "path": "index.html"
-    }
-  }
-}
-EOF
-
-# Create the index.html with a link to open IMSwitch
-cat << 'EOF' | sudo tee /usr/share/cockpit/imswitch/index.html > /dev/null
-<!DOCTYPE html>
-<html>
-<head>
-  <title>IMSwitch</title>
-</head>
-<body style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-  <a href="https://192.168.4.1:8001/imswitch/index.html?" target="_blank">
-    Open IMSwitch
-  </a>
-</body>
-</html>
-EOF
-
 echo "Installation complete. Access Cockpit at https://<IP-ADDRESS>:9090/"
